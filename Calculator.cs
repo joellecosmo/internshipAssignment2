408be67649d0478576ab99dac043ae60eadc8338
Joelle Cosmo <131820548+joellecosmo@users.noreply.github.com>
2024-12-27 13:13:05 +0300

    added the Calculator class

---
+++ Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace internshipAssignment2
+{
+    internal class Calculator
+    {
+        public int x { set; get; }
+        public int y { set; get; }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            while (y == 0)
+            {
+                Console.WriteLine("Cannot divide by zero! Enter another value:");
+                y = int.Parse(Console.ReadLine());
+            }
+            return x / y;
+        }
+    }
+    }
+
